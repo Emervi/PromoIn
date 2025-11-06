df506ce904afb0630cf5257d9fe14b4114bb4ea1
Emervi <janitraalvitoz@gmail.com>
2025-11-06 22:31:38 +0700

    Menambahkan data pembeli
    Data pembeli ini hanya fiktif dan untuk penugasan sajah

---
+++ data_pembeli.py
@@ -0,0 +1,10 @@
+pembeli = {
+    "id_pembeli": 1,
+    "nama": "Kylian Yamal",
+    "umur": 20,
+    "alamat": "Jl. Rumah Sakit",
+    "no_telp": "0821-2190-666",
+}
+
+for key, data in pembeli.items():
+    print(f"{key}: {data}")

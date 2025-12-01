4200ad35cd643796a9331434651b0e965add64a3
Emervi <janitraalvitoz@gmail.com>
2025-12-01 16:14:52 +0700

    Menghapus kembali agar enakeun

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import json
-
-from crud.umkm_crud import load_umkm
-from crud.food_vlogger_crud import load_fv
-
-umkms = load_umkm()
-fvs = load_fv()
-
-for data in umkms:
-    print(data)
-
-for data in fvs:
-    print(data)
+print("COK")

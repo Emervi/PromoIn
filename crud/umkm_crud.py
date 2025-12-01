6c23013f2ca55979feacf522d147f5447a3149d1
Emervi <janitraalvitoz@gmail.com>
2025-12-01 16:26:20 +0700

    Commit yang tidak perlu

--- crud/umkm_crud.py
+++ crud/umkm_crud.py
@@ -9,7 +9,42 @@ def save_umkm(data):
     with open(DATA_UMKM, "w") as f:
         json.dump(data, f, indent=4)
 
-def add_umkm(data_baru):
+def register_umkm():
     data = load_umkm()
+    
+    # input umkm baru
+    nama = input("Masukan nama anda: ")
+    email = input("Masukan email anda: ")
+    password = input("Masukan password anda: ")
+    
+    # generate id baru untuk data
+    id_baru = data[-1]["id"] + 1 if data else 1
+    
+    data_baru = {
+        "id": id_baru,
+        "nama": nama,
+        "email": email,
+        "password": password
+    }
+    
     data.append(data_baru)
     save_umkm(data)
+
+def login_umkm():
+    umkms = load_umkm()
+    
+    email = input("Masukan email: ")
+    password = input("Masukan password: ")
+    
+    for umkm in umkms:
+        print(umkm)
+        print(email)
+        print(password)
+        
+        if email == umkm["email"] and password == umkm["password"]:
+            print(email == umkm["email"])
+            print(password == umkm["password"])
+            print("BENER NJAY")
+            return True
+        elif email != umkm["email"] or password != umkm["password"]:
+            return print("Email atau password yang dimasukan salah.")

15952e550a4ff2225452240d0b5578c1b10b9261
Emervi <janitraalvitoz@gmail.com>
2025-12-15 16:52:44 +0700

    Tambah autentikasi Food Vlogger
    Menambahkan login, register Food Vlogger, dan memperbaiki beberapa kodingan

--- modules/autentikasi/autentikasi.py
+++ modules/autentikasi/autentikasi.py
@@ -1,7 +1,8 @@
-import json
-
 from modules.umkm.umkm import data_umkm, simpan_umkm
 from modules.umkm.home import beranda_umkm
+from modules.food_vlogger.food_vlogger import data_fv, simpan_fv
+from modules.food_vlogger.home import beranda_fv
+import data.session
 
 def cek_ketersedian_email(email):
     pass
@@ -18,27 +19,7 @@ def apakah_int(nilai):
 
 
 
-def pilih_peran():
-    print(data_umkm())
-    
-    print("\n===== BERANDA =====")
-    print("Silakan memilih peran yang sesuai dengan anda.")
-    print("1. UMKM")
-    print("2. Food Vlogger")
-    print("0. Keluar Sistem")
-    pilihan_user = int(input("Masukan pilihan anda: "))
-    
-    if pilihan_user == 1:
-        return "UMKM"
-    
-    elif pilihan_user == 2:
-        return "FV"
-    
-    else:
-        return 0
-
-
-
+# ======================================== UMKM ========================================
 def autentikasi_umkm():
     
     while True:
@@ -64,10 +45,7 @@ def autentikasi_umkm():
                 beranda_umkm()
             else:
                 login_umkm()
-        
-        elif pilihan_user == 3:
-            pass
-        
+                
         elif pilihan_user == 9:
             break
         
@@ -84,7 +62,7 @@ def register_umkm():
     while True:
         print("\n===== REGISTER UMKM =====")
         
-        # mengambil data umkm yang ada pada file umkm.json
+        # mengambil data umkm yang ada pada file umkm.csv
         umkms = data_umkm()
         email_umkms = [] # inisialisasi list untuk email umkm
         
@@ -156,6 +134,7 @@ def register_umkm():
         # menyimpan data akun
         simpan_umkm(data_baru)
         print("✅ Akun Berhasil Dibuat ✅")
+        data.session.USER_LOGIN = data_baru
         return True
 
 
@@ -165,7 +144,7 @@ def login_umkm():
     while True:
         print("\n===== LOGIN UMKM =====")
         
-        # mengambil data umkm yang ada pada file umkm.json
+        # mengambil data umkm yang ada pada file umkm.csv
         umkms = data_umkm()
         
         # meminta input email
@@ -204,6 +183,175 @@ def login_umkm():
         for umkm in umkms:
             
             if input_email == umkm[2] and input_password == umkm[3]:
+                return True
+            
+        print("❌ Email atau password salah ❌")
+
+
+
+# ======================================== FOOD VLOGGER (FV) ========================================
+def autentikasi_fv():
+    
+    while True:
+        print("\n===== LOGIN & REGISTER FOOD VLOGGER =====")
+        print("1. Register Food Vlogger")
+        print("2. Login Food Vlogger")
+        print("9. Kembali")
+        print("0. Keluar Sistem")
+        pilihan_user = int(input("Masukan pilihan anda: "))
+        
+        if pilihan_user == 1:
+            akun_fv = register_fv()
+            
+            if akun_fv:
+                beranda_fv()
+            else:
+                register_fv()
+            
+        elif pilihan_user == 2:
+            akun_fv = login_fv()
+            
+            if akun_fv:
+                beranda_fv()
+            else:
+                login_fv()
+                
+        elif pilihan_user == 9:
+            break
+        
+        elif pilihan_user == 0:
+            return "kill system"
+        
+        else:
+            print("Pilihan tidak ditemukan.")
+
+
+
+def register_fv():
+    
+    while True:
+        print("\n===== REGISTER FOOD VLOGGER =====")
+        
+        # mengambil data food vlogger yang ada pada file food_vlogger.csv
+        fvs = data_fv()
+        email_fvs = [] # inisialisasi list untuk email food vlogger
+        
+        # mengambil semua email food vlogger dan masukan ke dalam email_fvs
+        for fv in fvs:
+            email_fvs.append(fv[2])
+            
+        # meminta input nama
+        while True:
+            input_nama = input("Nama: ").capitalize().strip()
+            
+            # mengecek jika nama kosong
+            if not input_nama:
+                print("❌ Nama tidak boleh kosong ❌")
+                continue
+            break
+        
+        # meminta input email
+        while True:
+            input_email = input("Email: ").lower().strip()
+            
+            # mengecek jika email kosong
+            if not input_email:
+                print("❌ Email tidak boleh kosong ❌")
+                continue
+            
+            # mengecek format email
+            if '@' not in input_email or '.' not in input_email:
+                print("❌ Format email salah ❌")
+                continue
+            
+            # mengecek jika email sudah terdaftar
+            if input_email in email_fvs:
+                print("❌ Email sudah terdaftar ❌")
+                continue
+            break
+        
+        # meminta input password
+        while True:
+            input_password = input("Password: ").strip()
+            
+            # mengecek jika password kosong
+            if not input_password:
+                print("❌ Password tidak boleh kosong ❌")
+                continue
+            
+            # mengecek panjang password
+            if len(input_password) < 8:
+                print("❌ Panjang password kurang dari 8 karakter ❌")
+                continue
+            
+            konfirmasi_password = input("Konfirmasi Password: ").strip()
+
+            # mengecek password dengan konfirmasi password
+            if input_password != konfirmasi_password:
+                print("❌ Konfirmasi password tidak cocok ❌")
+                continue
+            break
+        
+        # generate id baru untuk data yang baru
+        if apakah_int(fvs[-1][0]):
+            id_baru = int(fvs[-1][0]) + 1
+        else:
+            id_baru = 1
+                                    
+        # memuat data akun
+        data_baru = [id_baru, input_nama, input_email, input_password]
+        
+        # menyimpan data akun
+        simpan_fv(data_baru)
+        print("✅ Akun Berhasil Dibuat ✅")
+        return True
+
+
+
+def login_fv():
+    
+    while True:
+        print("\n===== LOGIN FOOD VLOGGER =====")
+        
+        # mengambil data food vlogger yang ada pada file foof_vlogger.csv
+        fvs = data_fv()
+        
+        # meminta input email
+        while True:
+            input_email = input("Email: ").lower().strip()
+            
+            # mengecek jika email kosong
+            if not input_email:
+                print("❌ Email tidak boleh kosong ❌")
+                continue
+            
+            # mengecek format email
+            if '@' not in input_email or '.' not in input_email:
+                print("❌ Format email salah ❌")
+                continue            
+            break
+        
+        # meminta input password
+        while True:
+            input_password = input("Password: ").strip()
+            
+            # mengecek jika password kosong
+            if not input_password:
+                print("❌ Password tidak boleh kosong ❌")
+                continue
+            
+            # mengecek panjang password
+            if len(input_password) < 8:
+                print("❌ Panjang password kurang dari 8 karakter ❌")
+                continue
+            break
+        
+        print("Email: ", input_email)
+        print("Password: ", input_password)
+        
+        for fv in fvs:
+            
+            if input_email == fv[2] and input_password == fv[3]:
                 return True
             
         print("❌ Email atau password salah ❌")
--- modules/food_vlogger/home.py
+++ modules/food_vlogger/home.py
@@ -1,12 +1,9 @@
-import data.session
-
-def beranda_umkm():
+def beranda_fv():
     while True:
-        print("\n===== BERANDA UMKM =====")
+        print("\n===== BERANDA FOOD VLOGGER =====")
         print("0. Logout")
         print("Selamat datang, masih kosong cihuy!!")
         pilihan_user = int(input("> "))
         
         if pilihan_user == 0:
-            print(data.session.USER_LOGIN)
             break

f51a15cb30ebdb7a7819c1b6a4c8f9b74f49b3dc
WawanCoding18 <dzakwan112016>
2025-12-18 14:41:30 +0700

    menambahkan fitur lowongan berlangusng di beranda FV

--- modules/food_vlogger/home.py
+++ modules/food_vlogger/home.py
@@ -1,4 +1,5 @@
 from modules.food_vlogger.lowongan import lihat_lowongan
+from modules.food_vlogger.lowongan_berlangsung import lowongan_berlangsung
 from modules.utils import clear_screen
 
 def beranda_fv():
@@ -7,7 +8,8 @@ def beranda_fv():
         clear_screen()
         print("\n===== BERANDA FOOD VLOGGER =====")
         print("0. Logout")
-        print("1. Lihat lowongan")
+        print("1. Lihat lowongan🏪")
+        print("2. Lowongan Berlangsung🛎️")
         print("Selamat datang, masih kosong cihuy!!")
         pilihan_user = int(input("> "))
         #diambil atau belum diambil 
@@ -16,4 +18,7 @@ def beranda_fv():
 
         elif pilihan_user == 1:
             lihat_lowongan()
+
+        elif pilihan_user == 2:
+            lowongan_berlangsung()
         

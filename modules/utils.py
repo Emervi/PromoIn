d6bda90602b8d95e54e7bb48071b4f422234b371
Vowed69 <mhmmdarfa@student.upi.edu>
2026-01-01 21:44:43 +0700

    Mengubah password loggin menjadi bintang di beranda login UMKMi

--- modules/utils.py
+++ modules/utils.py
@@ -1,4 +1,5 @@
 import os
+import msvcrt
 
 # merapihkan tampilan sistem
 def clear_screen():
@@ -12,3 +13,19 @@ def apakah_int(nilai):
     
     except ValueError:
         return False
+
+def input_password(prompt="Password: "):
+    print(prompt, end="", flush=True)
+    pw = ""
+    while True:
+        ch = msvcrt.getch()
+        if ch in {b"\r", b"\n"}:  # Enter
+            print()
+            return pw
+        if ch == b"\x08":  # Backspace
+            if pw:
+                pw = pw[:-1]; print("\b \b", end="", flush=True)
+            continue
+        if ch in {b"\x00", b"\xe0"}:  # Tombol fungsi → abaikan
+            msvcrt.getch(); continue
+        pw += ch.decode(); print("*", end="", flush=True)

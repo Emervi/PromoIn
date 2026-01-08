ccc5bd913ad9351f167c85af7816f7fe235bd48e
Emervi <janitraalvitoz@gmail.com>
2026-01-09 00:52:55 +0700

    feat: fitur pembayaran lowongan oleh UMKM

--- modules/umkm/lowongan.py
+++ modules/umkm/lowongan.py
@@ -5,7 +5,7 @@ from data.config import DATA_LOWONGAN, DATA_FOOD_VLOGGER, DATA_LAMARAN
 from modules.umkm.lamaran import data_lamaran, update_lamaran
 from modules.food_vlogger.food_vlogger import data_fv
 from modules.bukti_promosi import data_bukti_promosi, tampilkan_bukti
-from modules.pembayaran import data_pembayaran
+from modules.pembayaran import data_pembayaran, lakukan_pembayaran
 import data.session
 
 def data_lowongan():
@@ -512,10 +512,17 @@ def kolaborasi():
             input("\nTekan ENTER untuk kembali...")
                     
         if perintah[0] == 'bayar':
-            print(f"BAYAR COK untuk id {perintah[1]}")
+            input_lowongan_id = perintah[1]
+            
+            hasil_bayar = lakukan_pembayaran(input_lowongan_id)
+            
+            if hasil_bayar == "bukti belum disetujui":
+                print("\n❌ Bukti belum disetujui ❌")
+            
+            if hasil_bayar == "dibayar":
+                print("\n✅ Pembayaran berhasil ✅")
             
             input("\nTekan ENTER untuk kembali...")
-            break
         
         if perintah[0] == "exit":
             break
